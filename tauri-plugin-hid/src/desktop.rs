79503013ae19aca87683aec4239ef910b18d6c52
Bukutsu <bukutsu@users.noreply.github.com>
2026-06-29 14:00:37 +0700

    Trim HID plugin comments

--- tauri-plugin-hid/src/desktop.rs
+++ tauri-plugin-hid/src/desktop.rs
@@ -20,8 +20,8 @@ pub fn init<R: Runtime, C: DeserializeOwned>(
 /// Access to the hid APIs.
 pub struct Hid<R: Runtime> {
     hid_api: Mutex<HidApi>,
-    device_list: Mutex<HashMap<String, hidapi::DeviceInfo>>, // Hashmap of devices using path as key. Updated on enumerate.
-    open_devices: Mutex<HashMap<String, hidapi::HidDevice>>, // Currently open devices.
+    device_list: Mutex<HashMap<String, hidapi::DeviceInfo>>,
+    open_devices: Mutex<HashMap<String, hidapi::HidDevice>>,
     _phantom: PhantomData<fn() -> R>,
 }
 
@@ -51,70 +51,48 @@ impl<R: Runtime> Hid<R> {
     }
 
     pub fn open(&self, path: &str) -> crate::Result<()> {
-        // Get a lock on the hid_api, device_list and open_devices mutex to ensure thread safety.
-        // This will panic if the mutex is poisoned, which should not happen in normal operation.
         let hid_api = self.hid_api.lock().unwrap();
         let device_list = self.device_list.lock().unwrap();
         let mut open_devices = self.open_devices.lock().unwrap();
-        // Attempt to open the device with by looking up path in the device_list HashMap.
+
         let device = device_list
             .get(path)
             .ok_or(crate::Error::HidDeviceNotFound)?;
-        // Check if the device is already open by looking it up in the open_devices HashMap.
         if open_devices.contains_key(path) {
             return Err(crate::Error::HidDeviceAlreadyOpen);
         }
-        // Open the device using the HidApi instance.
-        // The HidDevice instance is automatically closed when it goes out of scope.
+
         let open_device = device.open_device(&hid_api)?;
-        // Add the device to the open_devices HashMap.
         open_devices.insert(path.to_string(), open_device);
         Ok(())
     }
 
     pub fn close(&self, path: &str) -> crate::Result<()> {
-        // Get a lock on the open_devices mutex to ensure thread safety.
-        // This will panic if the mutex is poisoned, which should not happen in normal operation.
         let mut open_devices = self.open_devices.lock().unwrap();
-        // Remove the device from the open_devices HashMap using the path as the key.
-        // This will also close the device as the HidDevice is dropped.
         open_devices.remove(path);
         Ok(())
     }
 
     pub fn write(&self, path: &str, data: &[u8]) -> crate::Result<()> {
-        // Get a lock on the open_devices mutex to ensure thread safety.
-        // This will panic if the mutex is poisoned, which should not happen in normal operation.
         let open_devices = self.open_devices.lock().unwrap();
-        // Get device from the open_devices HashMap using the UUID as the key.
-        // If the device is not found, return an error.
         let device = match open_devices.get(path) {
             Some(device) => device,
             None => return Err(crate::Error::HidDeviceNotFoundInOpenDevices),
         };
-        // Write data to the device.
-        // TODO: Consider closing the device if write fails (to avoid stale device in list).
+
         device.write(data)?;
         Ok(())
     }
 
     pub fn read(&self, path: &str, timeout: i32) -> crate::Result<Vec<u8>> {
-        // Get a lock on the open_devices mutex to ensure thread safety.
-        // This will panic if the mutex is poisoned, which should not happen in normal operation.
         let open_devices = self.open_devices.lock().unwrap();
-        // Get device from the open_devices HashMap using the UUID as the key.
-        // If the device is not found, return an error.
         let device = match open_devices.get(path) {
             Some(device) => device,
             None => return Err(crate::Error::HidDeviceNotFoundInOpenDevices),
         };
         let mut buffer = vec![0; 64];
-        // Read data from the device into the buffer.
-        // The read method will block until data is available or an error occurs.
-        // TODO: Consider closing the device if read fails (to avoid stale device in list).
         let len = device.read_timeout(&mut buffer, timeout)?;
         buffer.truncate(len);
-        // Always return the buffer, even if empty
         Ok(buffer)
     }
 }
